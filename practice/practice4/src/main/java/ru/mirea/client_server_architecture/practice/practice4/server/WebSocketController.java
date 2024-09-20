5faa93d971a9c8dabcf5dfd186879fc42f46aacd
Alexander Gribkov <gribkovalexander@gmail.com>
2024-09-20 19:54:06 +0300

    practice 4: move html to spring

--- practice/practice4/src/main/java/ru/mirea/client_server_architecture/practice/practice4/server/WebSocketController.java
+++ practice/practice4/src/main/java/ru/mirea/client_server_architecture/practice/practice4/server/WebSocketController.java
@@ -3,6 +3,7 @@ package ru.mirea.client_server_architecture.practice.practice4.server;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebSocketController {
@@ -11,6 +12,11 @@ public class WebSocketController {
     @SendTo("/topic/return")
     public String processMessage(String message) {
         return message;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
 
